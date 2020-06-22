be5aa8cb952ff87620f8b559d54803996d24a8eb
Sean O Sullivan <seanfosullivan@gmail.com>
2020-06-22 12:09:36 +0100

    Adding homepage, admin and register page

---
+++ main/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
+# Create your views here.
+
+def homepage(request):
+	return render(request=request,
+				template_name="main/home.html",
+				context={"tutorials": Tutorial.objects.all})
+
+def register(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f"New Account Created: {username}")
+			login(request, user)
+			return redirect("main:homepage")
+		else:
+			for msg in form.error_messages:
+				messages.error(request, f"{msg}:{form.error_messages[msg]}")
+
+	form =  UserCreationForm
+	return render(request, 
+			"main/register.html", 
+			context={"form":form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "Logged out successfully!")
+	return redirect("main:homepage")
